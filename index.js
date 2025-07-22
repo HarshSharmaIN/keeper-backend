f14b77d32ac59ec4c48ab3f3a3f968ee8b065088
Harsh Sharma <120568101+HarshSharmaIN@users.noreply.github.com>
2025-07-22 08:32:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["https://keeper-frontend-five.vercel.app", "http://localhost:5173"],
+    origin: ["https://keeper-app-xi-six.vercel.app", "http://localhost:5173"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
